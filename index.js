bff9a5c20b5659db9c41096a0409fe548c2f7bed
Adrian Unger <dev@staydecent.ca>
2017-10-23 12:43:40 -0700

    Add missing exports.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import assoc from './src/assoc'
+import createAction from './src/createAction'
 import diffObj from './src/diffObj'
 import filter from './src/filter'
 import find from './src/find'
 import findIndex from './src/findIndex'
+import fromPairs from './src/fromPairs'
 import last from './src/last'
 import map from './src/map'
 import merge from './src/merge'
+import partial from './src/partial'
 import path from './src/path'
 import pathEq from './src/pathEq'
 import pathOr from './src/pathOr'
@@ -14,22 +17,25 @@ import pick from './src/pick'
 import pipe from './src/pipe'
 import range from './src/range'
 import reduce from './src/reduce'
+import request from './src/request'
 import some from './src/some'
 import toPairs from './src/toPairs'
-import fromPairs from './src/fromPairs'
 import uniq from './src/uniq'
 import where from './src/where'
 import without from './src/without'
 
 const wasmuth = {
   assoc,
+  createAction,
   diffObj,
   filter,
   find,
   findIndex,
+  fromPairs,
   last,
   map,
   merge,
+  partial,
   path,
   pathEq,
   pathOr,
@@ -38,9 +44,9 @@ const wasmuth = {
   pipe,
   range,
   reduce,
+  request,
   some,
   toPairs,
-  fromPairs,
   uniq,
   where,
   without
