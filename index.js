2d3403edcfb90229a4aa972b759461f156edd2ed
Adrian Unger <dev@staydecent.ca>
2018-02-19 15:30:58 -0800

    Add slugify.

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ import reduce from './src/reduce'
 import reject from './src/reject'
 import request from './src/request'
 import slice from './src/slice'
+import slugify from './src/slugify'
 import some from './src/some'
 import toPairs from './src/toPairs'
 import uniq from './src/uniq'
@@ -63,6 +64,7 @@ const wasmuth = {
   reject,
   request,
   slice,
+  slugify,
   some,
   toPairs,
   uniq,
