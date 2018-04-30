53734df0b5a831271b4b20d5a027c7389d2e7122
Adrian Unger <dev@staydecent.ca>
2018-04-30 11:50:58 -0700

    Update without to accept a single string key.
    Which is casted to an array.

--- src/without.js
+++ src/without.js
@@ -1,6 +1,10 @@
+import check from 'check-arg-types'
+
 import filter from './filter'
 import pick from './pick'
 import partial from './partial'
+
+const toType = check.prototype.toType
 
 /**
  * Return a new object without any of the specified keys included.
@@ -10,6 +14,10 @@ import partial from './partial'
  */
 export default function without (keys, obj) {
   if (arguments.length < 2) return partial(without, arguments)
+  check(arguments, [['string', 'array'], 'object'])
+  if (toType(keys) === 'string') {
+    keys = [keys]
+  }
   const keep = filter((k) => keys.indexOf(k) === -1, Object.keys(obj))
   return pick(keep, obj)
 }
