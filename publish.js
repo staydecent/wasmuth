36f8e2d4221e93c4483a554910eed595c1f2b2d0
Adrian Unger <dev@staydecent.ca>
2018-12-03 12:44:10 -0800

    Fix publish script.

--- publish.js
+++ publish.js
@@ -5,12 +5,12 @@ const {exec} = require('child_process')
 
 // $ npm run m-publish chunk
 if (require.main === module) {
-  if (process.argv.length < 2) {
+  if (process.argv.length < 3) {
     console.log('Not enough arguments supplied')
     process.exit(1)
   }
 
-  const name = process.argv[process.argv.length - 2]
+  const name = process.argv[process.argv.length - 1]
   const dir = resolve('packages/node_modules/@wasmuth', name)
 
   process.chdir(dir)
